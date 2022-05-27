01891fe5214441a640dbf61b2c1e479cf21d7882
ma-lijun <343190282@qq.com>
2022-05-27 14:54:28 +0800

    更新到最好预测效果的状态

--- my_model_12cat.py
+++ my_model_12cat.py
@@ -40,12 +40,9 @@ def default_loader(path):
 default_transform = transforms.Compose(
     [transforms.Resize(ORIGIN_SIZE, Image.BILINEAR),
      transforms.RandomCrop(INPUT_SIZE),
-     transforms.RandomHorizontalFlip(p=0.5),
-     transforms.RandomVerticalFlip(p=0.5),
-     # 0.3 的灰度
-     transforms.RandomGrayscale(p=0.3),
+     transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
-     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
      ]
 )
 
